7a472810c0654bb3e71da20b6ee4a44fb92a3be9
thole3287 <thole17062002@gmail.com>
2023-11-15 13:28:07 +0700

    [update] fillable user

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,11 +18,12 @@ class User extends Authenticatable implements JWTSubject
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    // protected $fillable = [
+    //     'name',
+    //     'email',
+    //     'password',
+    // ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
