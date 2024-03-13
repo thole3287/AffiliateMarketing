8af0983a729720caa17ad11ef938aac893504d7f
thole3287 <thole17062002@gmail.com>
2024-03-14 00:30:11 +0700

    fix url

--- app/Http/Controllers/Products/ProductsController.php
+++ app/Http/Controllers/Products/ProductsController.php
@@ -47,7 +47,8 @@ class ProductsController extends Controller
 
         // Upload and save product thumbnail
         if ($request->hasFile('product_thumbbail')) {
-            $image = $request->file('image');
+            $image = $request->file('product_thumbbail');
+            // dd()
             $imageName = time() . '.' . $image->getClientOriginalExtension();
             $image->move(public_path('storage/product_thumbnails'), $imageName); // Di chuyển ảnh vào thư mục public
             $imageUrl = asset('storage/product_thumbnails/' . $imageName);
@@ -116,7 +117,7 @@ class ProductsController extends Controller
 
         // Upload and save product thumbnail
         if ($request->hasFile('product_thumbbail')) {
-            $image = $request->file('image');
+            $image = $request->file('product_thumbbail');
             $imageName = time() . '.' . $image->getClientOriginalExtension();
             $image->move(public_path('storage/images/brands'), $imageName); // Di chuyển ảnh vào thư mục public
             $imageUrl = asset('storage/images/brands/' . $imageName);
