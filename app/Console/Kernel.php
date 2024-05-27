f1aa9c0a5ee67f74eb7c2b796465f54b35777add
thole3287 <thole17062002@gmail.com>
2024-05-27 23:57:58 +0700

    ess

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     ];
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:sync-elasticsearch-data-products')->everyMinute();
+        $schedule->command('app:sync-elasticsearch-data-products')->everyMinute()->withoutOverlapping();
         // $schedule->command('app:query-elastic-search')->everyFiveMinutes();
 
     }
